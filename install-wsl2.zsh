2a3fd08ca98ba96cc7c58985389a38a405672b29
Neurumaru <ksj6659@gmail.com>
2024-03-11 04:31:18 +0900

    feat: setup 추가

---
+++ install-wsl2.zsh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+apt-get install build-essential
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 
+
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+brew install gcc pyenv pyenv-virtualenv autojump
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-syntax-hightlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+cp ./.zshrc ~/.zshrc
+cp ./.p10k.zsh ~/.p10k.zsh
+cp -r ./.dotfiles ~/.dotfiles
